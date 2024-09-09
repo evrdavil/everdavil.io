af9c021903d395e2aa34c9731f36e031d1d9ad38
evrdavil <evrdavila09@gmail.com>
2024-09-09 13:57:38 -0600

    Add files via upload

---
+++ js/ordenar.js
@@ -0,0 +1,16 @@
+function ordena(){
+    *codigo para capturar el valor*
+    var a = parseInt{document.getElementById{"txtUno"}.value};
+    var b = parseInt{document.getElementById{"txtDos"}.value};
+    var c = parseInt{document.getElementById{"txtTres"}.value};
+    
+    if(a>b && a c){
+        'verdadero'
+    }
+     else{
+        'falso'
+     }
+
+     *codigo para mostrar*
+     document.getElementById("txtMayor").value = a
+}
